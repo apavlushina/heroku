52d0ea1c986dfa699e44bcf0385d50b0d4b6512f
apavlushina <ssemina7@gmail.com>
2019-11-06 11:09:56 +0100

    have routers and separation

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 4000;
+// app.listen(port, () => console.log("listening on port " + port));
+
+// const { Router } = require('express')
+// const router = new Router()
+// router.get('/', (request, response) => response.send('Welcome to the homepage!'))
+
 const express = require("express");
+
+const { Router } = express;
+const router = new Router();
+router.get("/", (request, response) =>
+  response.send("Welcome to the homepage!")
+);
+
 const app = express();
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log("listening on port " + port));
+app.use(router);
+
+const port = 4000;
+app.listen(port);
---
+++ router.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+
+const router = new Router();
+router.get("/", (request, response) => response.send("separated"));
+
+module.exports = router;
